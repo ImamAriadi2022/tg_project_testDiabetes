c5c5c3556e07a667881217ef12331a32767230d1
Imam ariadi <gamerapin139@gmail.com>
2024-06-12 00:54:44 +0700

    benerin python

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[19]:
+
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -8,86 +11,189 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-# Data collection and analysis
-# Pima diabetes dataset
 
-# Loading the diabetes dataset
+# Data collection and anality
+# PIma diabetes dataset
+# 
+
+# In[20]:
+
+
+# landing the diabetes data set
 diabetes_dataset = pd.read_csv('diabetes.csv')
 
-# Printing the first 5 rows
+
+# pd.read_csv?
+
+# In[21]:
+
+
+#printing time 5 rows
 diabetes_dataset.head()
 
-# Shape of the dataset
+
+# In[22]:
+
+
 diabetes_dataset.shape
 
-# Statistical measures
+
+# In[23]:
+
+
+#getting the statisitial
 diabetes_dataset.describe()
 
-# Value counts of the Outcome column
+
+# In[24]:
+
+
 diabetes_dataset['Outcome'].value_counts()
 
-# 0 = not diabetic, 1 = diabetic
 
-# Group by Outcome and calculate the mean
+# 0 = tidak diabetes
+# 1 = diabetes
+
+# In[25]:
+
+
 diabetes_dataset.groupby('Outcome').mean()
 
-# Separating the data and labels
-X = diabetes_dataset.drop(columns='Outcome', axis=1)
+
+# In[26]:
+
+
+X = diabetes_dataset.drop(columns = 'Outcome', axis =1)
 Y = diabetes_dataset['Outcome']
 
+
+# In[27]:
+
+
 print(X)
+
+
+# In[28]:
+
+
 print(Y)
 
-# Data standardization
+
+# data stadarisasi
+
+# In[29]:
+
+
 scaler = StandardScaler()
+
+
+# In[30]:
+
+
 scaler.fit(X)
+
+
+# In[31]:
+
+
 standardized_data = scaler.transform(X)
 
+
+# In[32]:
+
+
 print(standardized_data)
+
+
+# In[33]:
+
 
 X = standardized_data
 Y = diabetes_dataset['Outcome']
 
+
+# In[34]:
+
+
 print(X)
 print(Y)
 
-# Train test split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+# train test split
+
+# In[35]:
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+
+
+# In[36]:
+
 
 print(X.shape, X_train.shape, X_test.shape)
 
-classifier = svm.SVC(kernel='linear')
 
-# Training the Support Vector Machine Classifier
+# In[37]:
+
+
+classifier = svm.SVC (kernel='linear')
+
+
+# In[38]:
+
+
+#training the support vector Machine Classifier
 classifier.fit(X_train, Y_train)
 
-# Accuracy score on the training data
+
+# In[39]:
+
+
+# accuracy score on the training data
 X_train_prediction = classifier.predict(X_train)
-training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+training_data_accuracy =  accuracy_score(X_train_prediction, Y_train)
+
+
+# In[40]:
+
 
 print('Accuracy score of the training data : ', training_data_accuracy)
 
-# Accuracy score on the test data
+
+# In[42]:
+
+
+# accuracy score on the test data
 X_test_prediction = classifier.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
+
+# In[43]:
+
+
 print('Accuracy score of the test data : ', test_data_accuracy)
 
-# Input data
-input_data = (5, 166, 72, 19, 175, 25.8, 0.587, 51)
 
-# Mengubah input_data menjadi DataFrame dengan nama kolom yang sama
-input_data_as_df = pd.DataFrame([input_data], columns=X.columns)
+# In[45]:
 
-# Standarkan data input
-std_data = scaler.transform(input_data_as_df)
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardize the input data
+std_data = scaler.transform(input_data_reshaped)
 print(std_data)
 
-# Predicting
 prediction = classifier.predict(std_data)
 print(prediction)
 
 if (prediction[0] == 0):
-    print('The person is not diabetic')
+  print('The person is not diabetic')
 else:
-    print('The person is diabetic')
+  print('The person is diabetic')
+
